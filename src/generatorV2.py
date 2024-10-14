7aa721a50845f6982d771afebe16af6d633b4107
Youssef <yhamzawi2008@gmail.com>
2024-10-13 23:00:42 -0400

    Updated alcohol and smoking statusm, also added new datasets

--- src/generatorV2.py
+++ src/generatorV2.py
@@ -15,8 +15,10 @@ conditions = {
         "red_meat": (51, 10),  # Log-normal: Mean red meat intake = 51g/day, Std Dev = 10 
         "processed_meat": (38.9, 10),  # Log-normal: Mean processed meat intake = 38.9g/day, Std Dev = 10 
         "alcohol_use": (15, 4.5),  # Log-normal: Mean alcohol use = 15g/day, Std Dev = 4.5 
+        "drinking_status": [0.62, 0.38], # Distrubution of non-drinkers vs drinkers
         "smoking_duration": (12.9, 4.1),  # Log-normal: Mean smoking duration = 12.9 years, Std Dev = 4.1 
         "pack_years": (8, 5),  # Log-normal: Mean pack-years = 8, Std Dev = 5 
+        "smoking_status": [0.52, 0.48], # Distribution of non-smokers vs smokers
         "moderate_activity": (150, 40),  # Log-normal: Mean moderate physical activity = 150 min/week, Std Dev = 40 
         "vigorous_activity": (100, 40),  # Log-normal: Mean vigorous physical activity = 100 min/week, Std Dev = 40 
         "strength_activity": (3, 1),  # Log-normal: Mean strength-based activity = 3 times/week, Std Dev = 1
@@ -31,8 +33,10 @@ conditions = {
         "red_meat": (59.67, 10),  # Log-normal: Mean = 59.67g/day, Std Dev = 10
         "processed_meat": (58, 10),  # Log-normal: Mean = 58g/day, Std Dev = 10 
         "alcohol_use": (22.8, 11.4),  # Log-normal: Mean alcohol use = 22.8g/day, Std Dev = 11.4 
+        "drinking_status": [0.3578, 0.6422], # Distrubution of non-drinkers vs drinkers
         "smoking_duration": (16.705, 5.31),  # Log-normal: Mean smoking duration = 16.705 years, Std Dev = 5.31
         "pack_years": (20, 8),  # Log-normal: Mean pack-years = 20, Std Dev = 8 
+        "smoking_status": [0.36, 0.64], # Distribution of non-smokers vs smokers
         "moderate_activity": (60, 20),  # Log-normal: Mean moderate physical activity = 60 min/week, Std Dev = 20 
         "vigorous_activity": (30, 15),  # Log-normal: Mean vigorous physical activity = 30 min/week, Std Dev = 15 
         "strength_activity": (1, 0.5),  # Log-normal: Mean strength-based activity = 1 time/week, Std Dev = 0.5 
@@ -66,6 +70,8 @@ race_probs_controls = softmax(1 - incidence_values)
 sex_categories = [0, 1] # 0 for female 1 for male
 diabetes_categories = [0, 1] # 0 for without diabetes 1 for with diabetes
 famhx1_categories = [0, 1] # 0 for without family history 1 for with family history
+smoking_categories = [0, 1] # 0 for non-smokers  and 1 for smokers
+drinking_categories = [0, 1] # 0 for non-drinkers and 1 for drinkers
 races = [0, 1, 2, 3, 4] # 0 White, 1 Black, 2 Asian/PI, 3 American Indian, 4 Hispanic
 
 # Create intial mock for the dataset
@@ -141,28 +147,57 @@ def adjust_meat_intake(BMI, red_meat, processed_meat):
     
     return red_meat_adjusted, processed_meat_adjusted
 
-# Function to adjust smoking duration based on pack years
-def adjust_smoking_duration(pack_years, smoking_duration):
-    # Set boundaries for smoking duration
-    smoking_duration_adjusted = smoking_duration
+def adjust_alcohol_consumption(alcohol_use, drinking_status, sex, age):
+    if drinking_status == 0:
+        return 0
+
+    # Females drink roughly 1/3 of what males drink
+    if sex == 0:
+        alcohol_use *= 0.35
     
-    # Ensure consistency between smoking duration and pack years
-    if smoking_duration == 0:
-        pack_years = 0
-    elif pack_years == 0:
-        smoking_duration = 0
-    
-    if pack_years < 5:
-        smoking_duration_adjusted *= 0.75  # Decrease for low pack years
-    elif 5 <= pack_years < 15:
-        smoking_duration_adjusted *= 1.0  # No adjustment
-    else:
-        smoking_duration_adjusted *= 1.25  # Increase for high pack years
-    
-    # Ensure realistic bounds
-    smoking_duration_adjusted = min(max(smoking_duration_adjusted, 0), 40) 
-    
-    return smoking_duration_adjusted
+    # Those aged above 59 drink twice as less
+    if age > 59:
+        alcohol_use /= 2
+
+    return alcohol_use
+
+def adjust_smoking_duration_pack_years(smoking_duration, pack_years, smoking_status, age, sex, BMI, drinking_status, diabetes):
+    # Initialize variables
+    adjusted_smoking_duration = smoking_duration
+    adjusted_pack_years = pack_years
+
+    if smoking_status == 0:
+        return 0, 0  # Return a tuple of (0, 0)
+
+    # Age Adjustment
+    if age < 65:
+        adjusted_smoking_duration *= 0.9
+        adjusted_pack_years *= 0.9
+
+    # Sex Adjustment
+    if sex == 1:
+        adjusted_smoking_duration *= 1.1
+        adjusted_pack_years *= 1.1
+
+    # BMI Adjustment
+    if 18.5 <= BMI < 24.9:
+        adjusted_smoking_duration *= 0.95
+        adjusted_pack_years *= 0.95
+    elif BMI >= 30:
+        adjusted_smoking_duration *= 1.05
+        adjusted_pack_years *= 1.05
+
+    # Alcohol Consumption Adjustment
+    if drinking_status == 1:
+        adjusted_smoking_duration *= 1.1
+        adjusted_pack_years *= 1.1
+
+    # Diabetes Adjustment
+    if diabetes == 1:
+        adjusted_smoking_duration *= 1.2
+        adjusted_pack_years *= 1.2
+
+    return adjusted_smoking_duration, adjusted_pack_years  
 
 # Apply the functions to generate the additional variables
 for column_name, is_lognormal in [
@@ -180,72 +215,46 @@ for column_name, is_lognormal in [
 ]:
     df[column_name] = df.apply(generate_num_vars, axis=1, column_name=column_name, is_lognormal=is_lognormal)
 
+for column_name, category in [
+    ("sex", sex_categories),
+    ("diabetes", diabetes_categories),
+    ("famhx1", famhx1_categories),
+    ("smoking_status", smoking_categories),
+    ("drinking_status", drinking_categories),
+]:
+    df[column_name] = df.apply(generate_probability_vars, axis=1, column_name=column_name, category=category)
+
 # After generating the initial values for red_meat, processed_meat, and smoking_duration
 df['red_meat'], df['processed_meat'] = zip(*df.apply(
     lambda row: adjust_meat_intake(row['BMI'], row['red_meat'], row['processed_meat']), axis=1))
 
-df['smoking_duration'] = df.apply(
-    lambda row: adjust_smoking_duration(row['pack_years'], row['smoking_duration']), axis=1)
 
-for column_name, category in [
-    ("sex", sex_categories),
-    ("diabetes", diabetes_categories),
-    ("famhx1", famhx1_categories)
-]:
-    df[column_name] = df.apply(generate_probability_vars, axis=1, column_name=column_name, category=category)
-
-
-# Define smoking status probabilities by age group for controls (based on e-cigarette user data)
-smoking_status_probs_controls = {
-    "18-24": [0.163, 0.223, 0.614],  # [current, former, never]
-    "25-44": [0.306, 0.458, 0.236],
-    "45+": [0.427, 0.501, 0.072]
-}
-
-# Define smoking status probabilities for cases (based on CRC and smoking data)
-smoking_status_probs_cases = {
-    "men": [0.182, 0.518, 0.3],  # [current, former, never] for men
-    "women": [0.144, 0.296, 0.56]  # [current, former, never] for women
-}
-
-# Function to determine smoking status for controls based on age
-def determine_smoking_status_controls(age):
-    if age < 25:
-        return np.random.choice([2, 1, 0], p=smoking_status_probs_controls["18-24"])  # current = 2, former = 1, never = 0
-    elif age < 45:
-        return np.random.choice([2, 1, 0], p=smoking_status_probs_controls["25-44"])
-    else:
-        return np.random.choice([2, 1, 0], p=smoking_status_probs_controls["45+"])
-
-# Function to determine smoking status for cases based on sex
-def determine_smoking_status_cases(sex):
-    if sex == 0:  # Female
-        return np.random.choice([2, 1, 0], p=smoking_status_probs_cases["women"])
-    else:  # Male
-        return np.random.choice([2, 1, 0], p=smoking_status_probs_cases["men"])
-
-# Apply the functions to generate the smoking status column
-df['smoking_status'] = df.apply(
-    lambda row: determine_smoking_status_cases(row['sex']) if row['case_control'] == "1" else determine_smoking_status_controls(row['age']),
-    axis=1
+# Apply the alcohol adjustment function to the DataFrame
+df['alcohol_use'] = df.apply(
+    lambda row: adjust_alcohol_consumption(
+        row['alcohol_use'],
+        row['drinking_status'],
+        row['sex'],
+        row['age']     
+        ),
+        axis=1
 )
 
-# Adjust smoking-related variables based on smoking status
-def adjust_smoking_variables(row):
-    if row['smoking_status'] == 2:  # Current smoker
-        # No adjustment needed for current smokers
-        pass
-    elif row['smoking_status'] == 1:  # Former smoker
-        # Decrease smoking duration and pack years for former smokers
-        row['smoking_duration'] *= 0.8  # Example reduction factor
-        row['pack_years'] *= 0.8  # Example reduction factor
-    else:  # Never smoker
-        row['smoking_duration'] = 0
-        row['pack_years'] = 0
-    return row
+# Apply the smoking adjustment function to the DataFrame
+df['smoking_duration'], df['pack_years'] = zip(*df.apply(
+    lambda row: adjust_smoking_duration_pack_years(
+        row['smoking_duration'], 
+        row['pack_years'], 
+        row['smoking_status'], 
+        row['age'], 
+        row['sex'], 
+        row['BMI'], 
+        row['drinking_status'], 
+        row['diabetes']
+    ), 
+    axis=1
+))
 
-# Apply the adjustment function
-df = df.apply(adjust_smoking_variables, axis=1)
 
 # Define a function to generate race based on case_control status
 def generate_race(row):
